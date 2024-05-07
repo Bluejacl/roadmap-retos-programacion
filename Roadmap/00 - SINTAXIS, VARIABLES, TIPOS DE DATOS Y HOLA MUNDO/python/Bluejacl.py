e1a79ac14db7a30372f32f559f5f8450a7d62a8a
Bluejacl <josephqcastillo@gmail.com>
2024-05-06 19:44:54 -0500

    #00 - python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Bluejacl.py
@@ -0,0 +1,44 @@
+# https://www.python.org/
+
+# Comentario en linea
+
+"""
+Se pueden usar doble comillas o comillas sencillas
+"""
+
+'''
+comentario 2
+'''
+
+#variables
+my_variable="Se puede cambiar la variable"
+my_variable= "Python"
+
+PI=3.1416  # No existen contantes pero se puede representar asi.
+
+"""
+Datos Primitivos 
+"""
+
+my_str="hola"
+my_str2='hi'
+my_int=10
+my_float=10.3
+my_bool=True
+my_bool2=False
+
+# Se puede imprimir por terminal de diferentes maneras 
+print("Hola Python!") 
+print(my_variable)
+print (f"!Hola, {my_variable}!")
+
+
+# Para ver el tipo de dato se usa 'type'
+'''
+print(type(my_str))
+print(type(my_str2))
+print(type(my_int))
+print(type(my_float))
+print(type(my_bool))
+print(type(my_bool2))
+'''
